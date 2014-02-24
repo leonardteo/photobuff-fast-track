6de9ef6f78a6f5f5ddec9549fea349254600b6cc
Leonard Teo <leonard@ballistiq.com>
2014-02-24 13:42:18 -0500

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PhotobuffFastTrack::Application.config.secret_key_base = '77c6ebfb91c66fba276fcc95534f2cd4cd60c4ba7614e296b0d5280e7e09d551591a8335feb5d8ebf3d4af697931a2e123236217bf418d7998a9e65bde77b9ec'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PhotobuffFastTrack::Application.config.session_store :cookie_store, key: '_photobuff-fast-track_session'
