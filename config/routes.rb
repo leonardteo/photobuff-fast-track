cf6a77d62e7f8e5bddf3bbd3f4eb3d6cef3f5399
Leonard Teo <leonard@ballistiq.com>
2014-02-24 15:43:50 -0500

    Added bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,8 @@ PhotobuffFastTrack::Application.routes.draw do
 
   resources :photos
 
+  root "photos#index"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
