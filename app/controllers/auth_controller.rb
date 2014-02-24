d09ef2430ea3ec35042840a543c6b163dcd9b49d
Leonard Teo <leonard@ballistiq.com>
2014-02-24 16:43:54 -0500

    Added authentication

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -3,9 +3,21 @@ class AuthController < ApplicationController
   end
 
   def dologin
-    render :login
+    @user = User.find_by_email(params[:email]).try(:authenticate, params[:password])
+
+    if @user
+      session[:user_id] = @user.id
+      flash[:success] = "Welcome back #{@user.first_name}!"
+      redirect_to root_path
+    else
+      flash[:error] = "No user found with that email/password."
+      render :login
+    end
   end
 
   def logout
+    session[:user] = nil
+    flash[:success] = "Logged out"
+    redirect_to login_path
   end
 end
