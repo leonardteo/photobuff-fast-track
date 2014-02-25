d95d71017266c7ea3371249ea1ac44c6c25dfd00
Leonard Teo <leonard@ballistiq.com>
2014-02-25 10:18:28 -0500

    Added validations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -2,5 +2,12 @@ class Photo < ActiveRecord::Base
   belongs_to :user
   has_many :comments, :dependent => :destroy
 
+  # Validation
+  validates :user_id, :presence => true
+  validates :title, :presence => true
+  validates :description, :presence => true
+
+  # Paperclip
   has_attached_file :photo, :styles => { :large => "1280x1280>", :thumbnail => "640x640#" }
+  validates_attachment :photo, :presence => true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,10 @@ class User < ActiveRecord::Base
   has_many :comments, :dependent => :destroy
 
   has_secure_password
+
+  validates :first_name, :presence => true
+  validates :last_name, :presence => true
+  validates :email, :presence => true
+  validates :username, :presence => true
   
 end
